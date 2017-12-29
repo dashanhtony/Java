481a5e0799aed91b812754751be1b7742e928245
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2017-12-29 14:15:46 +0800

    add

---
+++ src/com/javalearn/test.java
@@ -0,0 +1,13 @@
+package com.javalearn;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017/12/29.
+ */
+public class test {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date)  ;
+    }
+}
