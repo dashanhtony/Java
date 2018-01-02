f5b8dd2ee0e9475c22e3c1fd6e38a722e8852458
shaoyuning@bestpay.com.cn <shaoyuning@bestpay.com.cn>
2018-01-02 09:52:04 +0800

    add

--- src/com/javalearn/test.java
+++ src/com/javalearn/test.java
@@ -9,5 +9,8 @@ public class test {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date)  ;
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
     }
 }
